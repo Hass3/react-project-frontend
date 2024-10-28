42e1c2cc0af48a58e6142683b6908a585211e8ba
Hass <hassmroueh@gmail.com>
2024-10-27 22:15:28 -0400

    Car form is now at the bottom

--- ruskos-auto-sale-app/src/pages/Home.js
+++ ruskos-auto-sale-app/src/pages/Home.js
@@ -1,17 +1,18 @@
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
 import Search from '../components/Search'
+import '../styles/Carform.css'
 import CarForm from '../components/CarForm'
 function Home(){
     
 
     return(
-       <>
+       <div className='home'>
         <NavBar />
         <Header />
         <Search />
         <CarForm />
-       </>
+       </div>
     )
 }
 
